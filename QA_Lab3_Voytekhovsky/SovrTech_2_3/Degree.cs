57fb638e5ea3a4a30c5a45217a3902ad22035ba4
vlad22rusal1 <162273869+vlad22rusal1@users.noreply.github.com>
2024-03-14 15:19:15 +0700

    Add files via upload

---
+++ QA_Lab3_Voytekhovsky/SovrTech_2_3/Degree.cs
@@ -0,0 +1,94 @@
+﻿using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+namespace SovrTech_2_3
+{
+
+    public class Degree
+    {
+        private int degree;
+        private int min;
+        private int sec;
+
+        public int Sec { 
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value >= 0 && value < 60)
+                {
+                    sec = value;
+                }
+            }
+        }
+        public void Init(int d, int m, int s)
+        {
+            degree = d;
+            min = m;
+            sec = s;
+        }
+        public void Display()
+        {
+            string s0, s1, s2, full;
+            s0 = Convert.ToString(degree);
+            s1 = Convert.ToString(min);
+            s2 = Convert.ToString(sec);
+            full = "Градусы :"+s0+" Минуты :" + s1 +" Секунды :"+ s2;
+            Console.WriteLine(full);
+        }
+        public void Read()
+        {
+            string s = "";
+            s = Console.ReadLine();
+            string[] s1;
+            s1 = s.Split(new char[] { ' ', '\t' });
+            degree = Convert.ToInt32(s1[0]);
+            min = Convert.ToInt32(s1[1]);
+            sec = Convert.ToInt32(s1[2]);
+        }
+        public int Rounding()
+        {
+            if (sec > 30)
+            {
+                min += 1;
+            }
+            if (min > 30)
+            {
+                degree += 1;
+            }
+            return degree;
+        }
+        public int Ndegree()
+        {
+            return degree;
+        }
+        public  Degree Add(Degree a, Degree b)
+        {
+            Degree sum;
+            sum = new Degree();
+            int k1, k2;
+            sum.degree = a.degree+b.degree;
+            k1 = a.min + b.min;
+            k2 = a.sec + b.sec;
+            if (k2 >= 60)
+            {
+                k1++;
+                k2 = k2 - 60;
+            }
+            sum.sec = k2;
+            if (k1 >= 60)
+            {
+                sum.degree++;
+                k1=k1 - 60;
+            }
+            sum.min = k1;
+            return sum;
+        }
+    }
+}
---
+++ QA_Lab3_Voytekhovsky/SovrTech_2_3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace SovrTech_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r;
+            Degree prop = new Degree();
+            prop.Sec = 36;
+            prop.Sec = 134;//prop.Sec осталось = 36
+            Degree a = new Degree();
+            a.Init(58, 31, 36);
+            a.Display();
+            r=a.Rounding();
+            Console.WriteLine("Градус после округления = "+r);
+            Console.WriteLine("\t");
+            Degree b = new Degree();
+            b.Read();
+            b.Display();
+            Console.WriteLine("\t");
+            int i, j;
+            Degree[] ms;
+            ms = new Degree[3];
+            for (i = 0; i < 2; i++)
+            {
+                ms[i] = new Degree();
+            }
+
+            ms[0].Init(190, 15, 23);
+            ms[0].Display();
+            ms[1].Init(190, 15, 20);
+            ms[1].Display();
+            
+            Console.WriteLine("\t");
+            Degree add =    new Degree();
+            ms[2] = add.Add(ms[0], ms[1]);
+            Console.WriteLine("Сложение градусов:");
+            ms[2].Display();
+            int[] c = new int[3];
+            for (i = 0; i < 2; i++)
+            {
+                c[i] = ms[i].Ndegree();
+            }
+            j = 0;
+            for(i=0; i<2; i++)
+            {
+                j += c[i];
+            }
+            Console.WriteLine(Convert.ToString(j));
+            Console.ReadKey();
+        }
+    }
+}
